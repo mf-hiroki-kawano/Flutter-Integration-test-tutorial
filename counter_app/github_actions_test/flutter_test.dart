34cb20676f43e33ab8b689ba6d2c8a0714afb52a
mf-hiroki-kawano <hiroki_kawano@mforce.co.jp>
2025-05-13 10:34:26 +0900

    test file for githubactions

---
+++ counter_app/github_actions_test/flutter_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'counter.dart';
+
+void main() {
+  group('Counter', () {
+    test('初期値は0', () {
+      final counter = Counter();
+      expect(counter.value, 0);
+    });
+
+    test('increment() を呼ぶと 1 増える', () {
+      final counter = Counter();
+      counter.increment();
+      expect(counter.value, 1);
+    });
+
+    test('decrement() を呼ぶと 1 減る', () {
+      final counter = Counter();
+      counter.decrement();
+      expect(counter.value, -1);
+    });
+  });
+}
