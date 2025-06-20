c4d7d3ec0bd1e72068f7b11ae7a1b8d72c178035
mf-akito-hirata <akito_hirata@mforce.co.jp>
2025-06-20 18:57:56 +0900

    [todo_app]fix detail page

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'todo_detail_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'task_index_manager.dart';
+import 'memo_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SharedPreferences.getInstance();
+  final prefs = await SharedPreferences.getInstance();
+
+  // todo_memo_で始まるキーを全て削除
+  final keys = prefs
+      .getKeys()
+      .where((k) => k.startsWith('todo_memo_'))
+      .toList();
+  for (final k in keys) {
+    await prefs.remove(k);
+  }
+
+  await TaskIndexManager().resetTaskIndexMap();
   runApp(MyApp());
 }
 
@@ -25,36 +38,47 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
-  final List<String> _todoList = [];
+  final List<Map<String, dynamic>> _todoList = [];
   final TextEditingController _textController = TextEditingController();
+  final TaskIndexManager _indexManager = TaskIndexManager();
+  final MemoRepository _memoRepository = SharedPreferencesMemoRepository();
 
-  void _addTodoItem(String task) {
+  Future<void> _addTodoItem(String task) async {
     if (task.isNotEmpty) {
+      int index = await _indexManager.assignIndexForTask(task);
       setState(() {
-        _todoList.add(task);
+        _todoList.add({'name': task, 'index': index});
       });
       _textController.clear();
     }
   }
 
-  void _removeTodoItem(int index) {
+  Future<void> _removeTodoItem(int listIndex) async {
+    final task = _todoList[listIndex];
+    final String taskName = task['name'];
+    final int taskIndex = task['index'];
+    await _indexManager.removeTask(taskName);
+    await _memoRepository.saveMemos(taskIndex, []); // メモも削除
     setState(() {
-      _todoList.removeAt(index);
+      _todoList.removeAt(listIndex);
     });
   }
 
-  Widget _buildTodoItem(String todoText, int index) {
+  Widget _buildTodoItem(Map<String, dynamic> task, int listIndex) {
     return ListTile(
-      title: Text(todoText),
+      title: Text(task['name']),
       trailing: IconButton(
         icon: Icon(Icons.delete, color: Colors.red),
-        onPressed: () => _removeTodoItem(index),
+        onPressed: () => _removeTodoItem(listIndex),
       ),
-      onTap: () {
+      onTap: () async {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => TodoDetailPage(todoText: todoText),
+            builder: (context) => TodoDetailPage(
+              taskIndex: task['index'],
+              todoText: task['name'],
+            ),
           ),
         );
       },
